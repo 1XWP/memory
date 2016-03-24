33cafed7c026b7afd010f2512e63e84d9aa4d0be
1XWP <katarzynaordon@gmail.com>
2016-03-24 16:41:38 +0100

    basic grid

---
+++ memory_game/Assets/Scripts/GameScipt.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+
+public class GameScipt : MonoBehaviour {
+
+    public GUISkin customSkin;
+    static int columns = 4;
+    static int rows = 4;
+    static int totalCards = columns * rows;
+    int matchesNeededToWin = totalCards / 2;
+    int machesMade = 0;
+    int cardWidth = 100;
+    int cardHeight = 100;
+    List<Card[]> arrayCards;
+    Card[,] gridOfCards;
+    ArrayList arrayCardsFlipped;
+    bool playerCanClick; //flag to prevent clicking
+    bool playerHasWon = false;
+
+    void Start()
+    {
+        playerCanClick = true;
+        arrayCards = new List<Card[]>();
+        gridOfCards = new Card[rows, columns];
+        arrayCardsFlipped = new ArrayList();
+
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < columns; j++)
+            {
+                gridOfCards[i, j] = new Card();
+            }
+        }
+    }
+
+    void OnGUI()
+    {
+        GUILayout.BeginArea(new Rect(0, 0, Screen.width, Screen.height));
+        BuildGrid();
+        GUILayout.EndArea();
+    }
+
+    private void BuildGrid()
+    {
+        GUILayout.BeginVertical();
+        for (int i = 0; i < rows; i++)
+        {
+            GUILayout.BeginHorizontal();
+            for (int j = 0; j < columns; j++)
+            {
+                Card card = gridOfCards[i, j];
+                if (GUILayout.Button(Resources.Load(card.img) as Texture2D, GUILayout.Width(cardWidth)))
+                {
+                    Debug.Log(card.img);
+                }
+            }
+            GUILayout.EndHorizontal();
+        }
+        GUILayout.EndVertical();
+    }
+
+    public class Card : System.Object
+    {
+        public bool isFaceUp = false;
+        public bool isMatched = false;
+        public string img;
+
+        public Card()
+        {
+            img = "lion";
+        }
+    }
+}
---
+++ memory_game/Assets/Scripts/TitleGUI.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TitleGUI : MonoBehaviour {
+
+    public GUISkin customSkin;
+
+    void OnGUI()
+    {
+        int buttonWidth = 100;
+        int buttonHeight = 50;
+        float halfScreenW = Screen.width * 0.5f;
+        float halfButtonW = buttonWidth * 0.5f;
+        GUI.skin = customSkin;
+        if(GUI.Button (new Rect(halfScreenW-halfButtonW, 250, buttonWidth, buttonHeight), "Play"))
+        {
+            Application.LoadLevel("gameScene");
+        }
+    }
+}
